30ccedfaedd63e1044c5b1481dd88640d8bd2ae8
alexander-lebed <alexanderlebed999@gmail.com>
2020-03-13 16:58:26 +0200

    fix Error style

--- layouts/ErrorPage/styles.ts
+++ layouts/ErrorPage/styles.ts
@@ -4,21 +4,21 @@ import { deviceMediaQuery } from '../../constants';
 export const Container = styled.div`
     height: 100%;
     display: flex;
-    flex-direction: column;
+    flex-direction: column-reverse;
     align-items: center;
-    /* justify-content: center; */
     background: url('/images/space-mobile.jpg') no-repeat center center fixed;
     background-size: cover;
     background-color: #02020c;
 
     @media ${deviceMediaQuery.laptop} {
+        flex-direction: column;
         background: url('/images/space-desktop.jpg') no-repeat center center fixed;
         background-color: #030003;
     }
 `;
 
 export const MessageText = styled.h3`
-    margin: 1em;
+    margin: 2em;
     text-align: center;
     color: orangered;
     font-family: Courier New;
